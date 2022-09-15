26afa995223105338dbc3777fbcdc5ee50f98e66
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-09-15 13:48:51 +0900

    Add Test Codes

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # supertest-sandobox
 
-How to run this app
+How to run this backend app
 
 ```
 npm i 
 npm start
 ```
 
-Then you can access "locahost:8080/test"
+Then you can access [locahost:8080](http://localhost:8080/)
+
+How to run test
+
+```
+npm test
+```
--- app.js
+++ app.js
@@ -2,8 +2,21 @@ import express from "express";
 
 const app = express();
 
-app.get("/test", (req, res) => {
-  res.send("test");
+// testsエンドポイント
+app.get("/", async (req, res) => {
+  res.send({ status: "running" });
+});
+
+// usersエンドポイント
+app.use(express.json());
+app.post("/users", async (req, res) => {
+  const { password, username } = req.body;
+  if (!password || !username) {
+    res.sendStatus(400);
+    return;
+  }
+
+  res.send({ userId: 0 });
 });
 
 export default app;
